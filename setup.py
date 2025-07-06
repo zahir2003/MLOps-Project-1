5d17c5650fe57fd10936bff3ac1805980997b8ce
Zahir <mohiduz03@gmail.com>
2025-07-06 14:28:39 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sk Mahiduzzaman",
+    author_email="skmahiduzzaman@gmail.com",
+    packages=find_packages()
+)
