92c81310609435ce45c49728561791c56b296d0b
Zahir <mohiduz03@gmail.com>
2025-07-07 13:23:40 +0530

    Experiment notebook added

--- demo.py
+++ demo.py
@@ -7,13 +7,13 @@
 # logging.error("This is an error message")
 # logging.critical("This is a critical message")
 
-# below the code is to check the exception handling
-from src.logger import logging
-from src.exception import MyException
-import sys
+# # below the code is to check the exception handling
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e,sys) from e
+# try:
+#     a = 1+'z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e,sys) from e
